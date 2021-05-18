9aa52e85e594dda639b91e61ecafe31a246a62d3
Yerald Mora <ymora@invercasa.com.ni>
2021-05-18 12:03:14 -0600

    Initial commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreMoviesAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+    }
+}
